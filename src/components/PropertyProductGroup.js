87da51836c6a99239673fc9e6f6ae8470d3821f6
Abi Noda <a@abinoda.com>
2017-04-23 23:58:29 -0700

    adds product group image preloading

--- src/components/PropertyProductGroup.js
+++ src/components/PropertyProductGroup.js
@@ -1,18 +1,39 @@
 import React, { Component } from 'react';
 
 class PropertyProductGroup extends Component {
+  componentWillMount() {
+    this.preloadImages()
+  }
+
+  preloadImages() {
+    const {products} = this.props;
+    const images = [];
+
+    this.groupProducts().map(id => {
+      const image = new Image();
+      image.src = products.byId[id].image;
+      
+      images.push(image);
+    });
+  }
+
+  groupProducts() {
+    const {products} = this.props;
+    const {groupSlug} = this.props.params;
+
+    return products.byGroup[groupSlug];
+  }
+
   handleAddProduct(productId) {
     this.props.methods.addProduct(this.props.propertyId, productId)
   }
 
   render() {
     const {products} = this.props;
-    const {groupSlug} = this.props.params;
-    const groupProducts = products.byGroup[groupSlug]
 
     return (
       <div>
-        {groupProducts.map(id => {
+        {this.groupProducts().map(id => {
           return (
             <div key={id}>
               {products.byId[id].device_name}
