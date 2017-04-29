630e8652d43207d9aa54a4157cd2907ac8881e31
Abi Noda <a@abinoda.com>
2017-04-29 01:03:14 -0500

    adds checklist blank state cta

--- src/components/PropertyChecklist.js
+++ src/components/PropertyChecklist.js
@@ -25,24 +25,37 @@ class PropertyChecklist extends Component {
 
     return (
       <div className="PropertyChecklist-container">
-        <div className="PropertyChecklist-products">
-          {property.products.map(id => {
-            return (
-              <div key={id}>
-                <Link className="product" to={`/property/${propertyId}/product/${id}`}>
-                  {products.byId[id].device_name}
-                </Link>
-              </div>
-            );
-          })}
+        {property.products.length > 0 &&
+          <div>
+            <div className="PropertyChecklist-products">
+              {property.products.map(id => {
+                return (
+                  <div key={id}>
+                    <Link className="product" to={`/property/${propertyId}/product/${id}`}>
+                      {products.byId[id].device_name}
+                    </Link>
+                  </div>
+                );
+              })}
 
-          <Link className="add-more" to={`/property/${propertyId}/productGroups`}>Add more</Link>
-        </div>
+              <Link className="add-more" to={`/property/${propertyId}/productGroups`}>Add more</Link>
+            </div>
 
-        <div className="actions">
-          <Link to={`/property/${propertyId}/share`}>Share</Link>
-          <Link to={`/property/${propertyId}/edit`}>Edit</Link>
-        </div>
+            <div className="actions">
+              <Link to={`/property/${propertyId}/share`}>Share</Link>
+              <Link to={`/property/${propertyId}/edit`}>Edit</Link>
+            </div>
+          </div>
+        }
+
+        {property.products.length == 0 &&
+          <div className="PropertyChecklist-products">
+            <div className="actions blank-state">
+              <Link className="add-more" to={`/property/${propertyId}/productGroups`}>Add products to list</Link>
+            </div>
+          </div>
+        }
+
       </div>
     );
   }
