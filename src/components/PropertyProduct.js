fc244d39d58e89fadd3d2e44ca9137bbea48fcce
Abi Noda <a@abinoda.com>
2017-05-09 23:37:36 -0700

    adds product details screen

--- src/components/PropertyProduct.js
+++ src/components/PropertyProduct.js
@@ -17,9 +17,24 @@ class PropertyProduct extends Component {
     const product = products.byId[productId];
 
     return (
-      <div>
-        {product.device_name}
-        <button onClick={() => this.handleDeleteProduct()}>Remove</button>
+      <div className="PropertyProduct-container">
+        <div className="product">
+          <div className="product-view">
+            <div className="product-header">
+              <span className="product-name">{product.device_name}</span>
+            </div>
+            <div className="product-image">
+              <img alt={product.device_name} src={product.image} width="60%" />
+            </div>
+            <div className="product-info">
+              {product.website && <a href={product.website} target="_blank">Device Website &raquo;</a>}
+              {product.support && <a href={product.support} target="_blank">Device Support &raquo;</a>}
+            </div>
+            <div className="actions">
+              <button className="button" onClick={() => this.handleDeleteProduct()}>Remove from list</button>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
