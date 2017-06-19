bf76cc94404453cabe348dcee91f1da52e40cb2c
John O'Sullivan <josullivan1@luc.edu>
2017-06-19 10:02:05 -0500

    Fixed the routes issue

--- build_sw.js
+++ build_sw.js
@@ -1,7 +1,19 @@
 var swPrecache = require('sw-precache');
 var rootDir = 'build';
+/* Callback for the sw-precache results */
 function swPrecache_callback(error) { console.log("Error:" + error) }
+/* Writes the worker.js service file into the build dir */
 swPrecache.write(`${rootDir}/worker.js`, {
   staticFileGlobs: [rootDir + '/**/*.{js,html,css,png,jpg,gif,svg,eot,ttf,woff,json}'],
-  stripPrefix: rootDir
+  stripPrefix: rootDir,
+  runtimeCaching: [{
+  urlPattern: /\/property\//,
+  handler: 'fastest',
+  options: {
+    cache: {
+      maxEntries: 10,
+      name: 'property-cache'
+    }
+  }
+  }]
 }, swPrecache_callback);
