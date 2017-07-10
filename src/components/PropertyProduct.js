5a9c6405cae1686bd43ad05cef2ec4cb74d6cc87
John O'Sullivan <josullivan1@luc.edu>
2017-07-10 10:25:56 -0500

    Add Link to SHDB

--- src/components/PropertyProduct.js
+++ src/components/PropertyProduct.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { browserHistory } from 'react-router'
 
-class PropertyProduct extends Component {  
+class PropertyProduct extends Component {
   handleDeleteProduct() {
     const {methods, propertyId} = this.props;
     const {productId} = this.props.params;
@@ -15,7 +15,6 @@ class PropertyProduct extends Component {
     const {productId} = this.props.params;
     const {products} = this.props;
     const product = products.byId[productId];
-
     return (
       <div className="PropertyProduct-container">
         <div className="product">
@@ -28,6 +27,7 @@ class PropertyProduct extends Component {
                 <img alt={product.device_name} src={product.image} width="60%" />
               </div>
               <div className="product-info">
+                {product.shdb_url && <a href={product.shdb_url} target="_blank">View on SmartHomeDB &raquo;</a>}
                 {product.website && <a href={product.website} target="_blank">Device Website &raquo;</a>}
                 {product.support && <a href={product.support} target="_blank">Device Support &raquo;</a>}
               </div>
